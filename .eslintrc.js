b408ecce9291622efd757b8e63db7bcde721dfaa
oma817 <oma@fifteenh.io>
2024-07-22 19:56:52 +0900

    feat: eslintrc 파일에 들어가는 설정에 대한 설명

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
+  // 내부적인 설정파일을 만들어둔것을 사용하기 위한 속성
+  // node_modules 밑에 @next 밑에 rules 폴더에 각각의 룰이 설정 되어있음
   extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
-  // plugins: ['prettier', 'unused-imports'],
   plugins: ['prettier'],
   rules: {
     // 선언되지 않은 변수 또는 임포트 구문 정리 규칙
     'no-undef': 'error',
-    // 'unused-imports/no-unused-imports': 'error',
 
     // 프리티어 설정
+    // 해당 설정을 error로 두었기 때문에 저장 버튼 이용시
+    // 자동으로 문법을 수정해준다
     'prettier/prettier': [
       'error',
       // 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
